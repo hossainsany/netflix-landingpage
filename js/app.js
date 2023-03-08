5d8ae53d9dcb77b17e08cfab3b6c453c191d87bc
Hossain Sany <hossainsany3@gmail.com>
2023-03-08 23:38:21 +0600

    added disclaimer

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 let accordionBtn = document.querySelectorAll(".accordion-btn");
 let accordionContent = document.querySelectorAll(".accordion-content");
+let disclaimerBtn = document.querySelector("#disclaimer-btn");
+let disclaimer = document.querySelector(".disclaimer");
 
 for (let i = 0; i < accordionBtn.length; i++) {
-	accordionBtn[i].addEventListener("click", () => {
-		if (accordionContent[i].classList.contains("accordion-content-active")) {
-			accordionContent[i].classList.remove("accordion-content-active");
-		} else {
-			accordionContent[i].classList.add("accordion-content-active");
-		}
-	});
+    accordionBtn[i].addEventListener("click", () => {
+        if (accordionContent[i].classList.contains("accordion-content-active")) {
+            accordionContent[i].classList.remove("accordion-content-active");
+        } else {
+            accordionContent[i].classList.add("accordion-content-active");
+        }
+    });
 }
+
+disclaimerBtn.addEventListener("click", () => {
+    if (disclaimer.classList.contains("close")) {
+        disclaimer.classList.remove("close");
+    } else {
+        disclaimer.classList.add("close");
+    }
+});
